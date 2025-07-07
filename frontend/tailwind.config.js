3e8d170aa37e2e3113537aa8de26aff540c59e80
hardik-47 <jainhardik929@gmail.com>
2025-07-07 15:12:50 +0530

    css done

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './components/**/*.{js,vue}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './app.vue'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
